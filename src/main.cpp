045c230e4c6edaa88bf10da953b45c2e20e44798
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-06-07 16:21:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,58 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
+#include <ctime>
+#include <chrono>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
+  std::srand(std::time(0));
+  const int l = 60;
+  
+  Train train1;
+  int count = l;
   while (count--)
-    train.addCar(false);
+    train1.addCar(false);
+  
+  auto start1 = std::chrono::high_resolution_clock::now();
+  int length1 = train1.getLength();
+  auto end1 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> time1 = end1 - start1;
+  
+  std::cout << "off" << std::endl;
+  std::cout << "length: " << length1 << std::endl;
+  std::cout << "operations: " << train1.getOpCount() << std::endl;
+  std::cout << "time: " << time1.count() << "s" << std::endl << std::endl;
+  
 
-  std::cout << "a" << train.getLength() << std::endl;
-  std::cout << "b" << train.getOpCount() << std::endl;
+  Train train2;
+  count = l;
+  while (count--)
+    train2.addCar(std::rand() % 2 == 0);
+  
+  auto start2 = std::chrono::high_resolution_clock::now();
+  int length2 = train2.getLength();
+  auto end2 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> time2 = end2 - start2;
+  
+  std::cout << "random" << std::endl;
+  std::cout << "length: " << length2 << std::endl;
+  std::cout << "operations: " << train2.getOpCount() << std::endl;
+  std::cout << "time: " << time2.count() << "s" << std::endl << std::endl;
+
+  Train train3;
+  count = l;
+  while (count--)
+    train3.addCar(true);
+  
+  auto start3 = std::chrono::high_resolution_clock::now();
+  int length3 = train3.getLength();
+  auto end3 = std::chrono::high_resolution_clock::now();
+  std::chrono::duration<double> time3 = end3 - start3;
+  
+  std::cout << "on" << std::endl;
+  std::cout << "length: " << length3 << std::endl;
+  std::cout << "operations: " << train3.getOpCount() << std::endl;
+  std::cout << "time: " << time3.count() << "s" << std::endl;
+
   return 0;
 }
