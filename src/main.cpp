9ed8eeb6d1f2815901b748841e561c6c06062247
Андрей <128510774+andrey-99@users.noreply.github.com>
2025-06-07 16:30:43 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,56 +6,56 @@
 #include "train.h"
 
 int main() {
-  std::srand(std::time(0));
-  const int l = 60;
+    std::srand(std::time(0));
+    const int l = 60;
 
-  Train train1;
-  int count = l;
-  while (count--)
-    train1.addCar(false);
+    Train train1;
+    int count = l;
+    while (count--)
+        train1.addCar(false);
 
-  auto start1 = std::chrono::high_resolution_clock::now();
-  int length1 = train1.getLength();
-  auto end1 = std::chrono::high_resolution_clock::now();
-  auto time1 = std::chrono::duration_cast<std::chrono::milliseconds>(
-      end1 - start1);
+    auto start1 = std::chrono::high_resolution_clock::now();
+    int length1 = train1.getLength();
+    auto end1 = std::chrono::high_resolution_clock::now();
+    auto time1 = std::chrono::duration_cast<std::chrono::microseconds>(
+        end1 - start1);
 
-  std::cout << "off" << std::endl;
-  std::cout << "length: " << length1 << std::endl;
-  std::cout << "operations: " << train1.getOpCount() << std::endl;
-  std::cout << "time: " << time1.count() << " мс" << std::endl << std::endl;
+    std::cout << "off" << std::endl;
+    std::cout << "length: " << length1 << std::endl;
+    std::cout << "operations: " << train1.getOpCount() << std::endl;
+    std::cout << "time: " << time1.count() << " ms" << std::endl << std::endl;
 
-  Train train2;
-  count = l;
-  while (count--)
-    train2.addCar(std::rand() % 2 == 0);
+    Train train2;
+    count = l;
+    while (count--)
+        train2.addCar(std::rand() % 2 == 0);
 
-  auto start2 = std::chrono::high_resolution_clock::now();
-  int length2 = train2.getLength();
-  auto end2 = std::chrono::high_resolution_clock::now();
-  auto time2 = std::chrono::duration_cast<std::chrono::milliseconds>(
-      end2 - start2);
+    auto start2 = std::chrono::high_resolution_clock::now();
+    int length2 = train2.getLength();
+    auto end2 = std::chrono::high_resolution_clock::now();
+    auto time2 = std::chrono::duration_cast<std::chrono::microseconds>(
+        end2 - start2);
 
-  std::cout << "random" << std::endl;
-  std::cout << "length: " << length2 << std::endl;
-  std::cout << "operations: " << train2.getOpCount() << std::endl;
-  std::cout << "time: " << time2.count() << " мс" << std::endl << std::endl;
+    std::cout << "random" << std::endl;
+    std::cout << "length: " << length2 << std::endl;
+    std::cout << "operations: " << train2.getOpCount() << std::endl;
+    std::cout << "time: " << time2.count() << " ms" << std::endl << std::endl;
 
-  Train train3;
-  count = l;
-  while (count--)
-    train3.addCar(true);
+    Train train3;
+    count = l;
+    while (count--)
+        train3.addCar(true);
 
-  auto start3 = std::chrono::high_resolution_clock::now();
-  int length3 = train3.getLength();
-  auto end3 = std::chrono::high_resolution_clock::now();
-  auto time3 = std::chrono::duration_cast<std::chrono::milliseconds>(
-      end3 - start3);
+    auto start3 = std::chrono::high_resolution_clock::now();
+    int length3 = train3.getLength();
+    auto end3 = std::chrono::high_resolution_clock::now();
+    auto time3 = std::chrono::duration_cast<std::chrono::microseconds>(
+        end3 - start3);
 
-  std::cout << "on" << std::endl;
-  std::cout << "length: " << length3 << std::endl;
-  std::cout << "operations: " << train3.getOpCount() << std::endl;
-  std::cout << "time: " << time3.count() << " мс" << std::endl;
+    std::cout << "on" << std::endl;
+    std::cout << "length: " << length3 << std::endl;
+    std::cout << "operations: " << train3.getOpCount() << std::endl;
+    std::cout << "time: " << time3.count() << " ms" << std::endl;
 
-  return 0;
+    return 0;
 }
